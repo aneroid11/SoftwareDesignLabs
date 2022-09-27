7512f39cd1929ce65ac727eb518a3e5682ddbe68
aneroid11 <a-e-v2003@yandex.by>
2022-09-27 10:40:54 +0300

    add units coefficients

--- Converter/app/src/main/java/com/example/converter/DataViewModel.kt
+++ Converter/app/src/main/java/com/example/converter/DataViewModel.kt
@@ -9,11 +9,41 @@ class DataViewModel : ViewModel() {
     private val _sourceValue: MutableLiveData<Double> = MutableLiveData<Double>(0.0)
     private val _destinationValue: MutableLiveData<Double> = MutableLiveData<Double>(0.0)
 
-    private var _units: Map<String, Array<String>> = mapOf(
-        "currency" to arrayOf("dollars (US)", "euro", "BYN"),
-        "mass" to arrayOf("pounds", "kilograms", "carats"),
-        "distance" to arrayOf("meters", "inches", "feet")
+    private val _unitsCoefficients: Map<String, Map<String, Double>> = mapOf(
+        "currency" to mapOf(
+            "dollars (US)" to 1.0,
+            "euro" to 0.96,
+            "BYN" to 0.396
+        ),
+        "mass" to mapOf(
+            "kilograms" to 1.0,
+            "pounds" to 0.4535924,
+            "ounces" to 0.02834952
+        ),
+        "distance" to mapOf(
+            "meters" to 1.0,
+            "inches" to 0.0254,
+            "yards" to 0.9144
+        )
     )
+
+    private fun unitsCoefficientsToUnits(): Map<String, Array<String>> {
+        val unitsMap = mutableMapOf<String, Array<String>>()
+
+        for (unitsType in _unitsCoefficients.keys) {
+            val unitsArray = _unitsCoefficients[unitsType]!!.keys.toTypedArray()
+            unitsMap[unitsType] = unitsArray
+        }
+
+        return unitsMap
+    }
+
+    private var _units: Map<String, Array<String>> = unitsCoefficientsToUnits()
+    /*private var _units: Map<String, Array<String>> = mapOf(
+        "currency" to arrayOf("dollars (US)", "euro", "BYN"),
+        "mass" to arrayOf("kilograms", "pounds", "carats"),
+        "distance" to arrayOf("meters", "inches", "feet")
+    )*/
 
     private var _unitsType: String = _units.keys.toTypedArray()[0]
     private var _sourceUnits: String = _units[_unitsType]!![0]
