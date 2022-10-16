a15c2f7382a9fd6251b61472ddbaa4b7ecedb9db
aneroid11 <a-e-v2003@yandex.by>
2022-10-16 20:30:47 +0300

    checking for invalid characters

--- Converter/app/src/main/java/com/example/converter/MonitoringEditText.kt
+++ Converter/app/src/main/java/com/example/converter/MonitoringEditText.kt
@@ -1,10 +1,14 @@
 package com.example.converter
 
+import android.content.ClipboardManager
 import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
 import android.util.AttributeSet
 import android.widget.EditText
 //import android.widget.EditText
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.getSystemService
 
 class MonitoringEditText : androidx.appcompat.widget.AppCompatEditText {
     private var monContext: Context
@@ -27,23 +31,52 @@ class MonitoringEditText : androidx.appcompat.widget.AppCompatEditText {
         val consumed: Boolean = super.onTextContextMenuItem(id)
 
         when (id) {
-            android.R.id.cut -> onTextCut()
-            android.R.id.copy -> onTextCopy()
+            //android.R.id.cut -> onTextCut()
+            //android.R.id.copy -> onTextCopy()
             android.R.id.paste -> onTextPaste()
         }
 
         return consumed
     }
 
-    private fun onTextCut() {
+    /*private fun onTextCut() {
         Toast.makeText(context, "Cut!", Toast.LENGTH_SHORT).show()
     }
 
     private fun onTextCopy() {
         Toast.makeText(context, "Copy!", Toast.LENGTH_SHORT).show()
+    }*/
+
+    private fun checkForDigitsAndDots(str: String): Boolean {
+        for (ch: Char in str) {
+            if (ch != '.' && !ch.isDigit()) {
+                return false
+            }
+        }
+
+        return true
     }
 
     private fun onTextPaste() {
-        Toast.makeText(context, "Paste!", Toast.LENGTH_SHORT).show()
+        val clipboardManager: ClipboardManager =
+            context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+        val pData = clipboardManager.primaryClip
+        val item = pData!!.getItemAt(0)
+        val txtPaste: String = item.text.toString()
+
+        val txtCurrent: String = text.toString()
+
+        if (checkForDigitsAndDots(txtPaste)) {
+            
+        }
+        else {
+            Toast.makeText(
+                context,
+                "the text you're trying to paste has invalid characters!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        //Toast.makeText(context, "Paste!", Toast.LENGTH_SHORT).show()
     }
 }
