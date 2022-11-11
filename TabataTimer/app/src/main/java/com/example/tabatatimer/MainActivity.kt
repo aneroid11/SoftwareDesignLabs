4662fa17ff51add1e6d6415dfe17ae3304200c79
aneroid11 <a-e-v2003@yandex.by>
2022-11-11 11:49:48 +0300

    add settings activity and dummy settings button

--- TabataTimer/app/src/main/java/com/example/tabatatimer/MainActivity.kt
+++ TabataTimer/app/src/main/java/com/example/tabatatimer/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.tabatatimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_activity_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.settings_item) {
+            Log.d("MainActivity","settings button pressed")
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
