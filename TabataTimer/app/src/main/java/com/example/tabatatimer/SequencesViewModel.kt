8e15f744058b5b1b8ca4b0e8cb840cf1ba0d37fb
aneroid11 <a-e-v2003@yandex.by>
2022-11-15 21:57:31 +0300

    reading from sequences.json

--- TabataTimer/app/src/main/java/com/example/tabatatimer/SequencesViewModel.kt
+++ TabataTimer/app/src/main/java/com/example/tabatatimer/SequencesViewModel.kt
@@ -1,12 +1,20 @@
 package com.example.tabatatimer
 
+import android.app.Application
 import android.graphics.Color
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.lang.Exception
 import kotlin.random.Random
 
-class SequencesViewModel : ViewModel() {
+class SequencesViewModel(app: Application) : AndroidViewModel(app) {
     private val _sequencesList: MutableLiveData<MutableList<Sequence>> =
         MutableLiveData<MutableList<Sequence>>(mutableListOf())
 
@@ -22,7 +30,40 @@ class SequencesViewModel : ViewModel() {
 
     init {
         // load the sequences list from .json file
-        for (i in 1..10) {
+        val context = getApplication<Application>().applicationContext
+
+        val file = File(context.filesDir, "sequences.json")
+
+        if (file.exists()) {
+            Log.d("SequencesViewModel", "file exists")
+            // read it
+
+            val fileReader = FileReader(file)
+            val bufferedReader = BufferedReader(fileReader)
+            val contents: List<String> = bufferedReader.readLines()
+            var contentsStr = ""
+
+            for (line in contents) {
+                contentsStr += line + "\n"
+            }
+
+            try {
+                val sequencesArray = Gson().fromJson(contentsStr, Array<Sequence>::class.java)
+
+                for (seq in sequencesArray) {
+                    _sequencesList.value!!.add(seq)
+                }
+            }
+            catch (e: Exception) {
+                Log.d("SequencesViewModel", "sequences.json is empty or corrupted!")
+            }
+        }
+        else {
+            file.createNewFile()
+            Log.d("SequencesViewModel", "file does not exist. creating a new file")
+        }
+
+        /*for (i in 1..10) {
             _sequencesList.value!!.add(
                 Sequence(
                     getRandomColor(),
@@ -40,6 +81,6 @@ class SequencesViewModel : ViewModel() {
                     )
                 )
             )
-        }
+        }*/
     }
 }
